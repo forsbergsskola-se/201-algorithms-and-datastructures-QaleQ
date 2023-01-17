72586334a050e7387ed774b2aae12888a0f797f4
QaleQ <aleflard@gmail.com>
2023-01-17 12:21:22 +0100

    2 Iterator Pattern

--- P201-Algorithms-And-DataStructures/TurboCollections.Test/UnitTest1.cs
+++ P201-Algorithms-And-DataStructures/TurboCollections.Test/UnitTest1.cs
@@ -1,25 +1,51 @@
 namespace TurboCollections.Test;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
-
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
-}
-
 public static class MathsTests
 {
     [Test]
     public static void SayHelloExists()
     {
-        TurboCollections.TurboMaths.SayHello();
+        TurboMaths.SayHello();
         Assert.Pass();
     }
+}
+
+public class IteratorTests
+{
+    [Test]
+    [TestCase(12)]
+    [TestCase(15)]
+    [TestCase(-1)]
+    [TestCase(0)]
+    public void GetEvenNumbersListTest(int maxNumber)
+    {
+        List<int> testList = TurboMaths.GetEvenNumbersList(maxNumber);
+        
+        if (testList.Count > maxNumber / 2 + 1) Assert.Fail();
+        
+        foreach (int num in testList)
+            if (num % 2 != 0 || num < 0 || num > maxNumber) Assert.Fail();
+    }
+    
+    
+    [Test]
+    [TestCase(12)]
+    [TestCase(15)]
+    [TestCase(-1)]
+    [TestCase(0)]
+    public void GetEvenNumbersTest(int maxNumber)
+    {
+        List<int> testList = new();
+        foreach (int number in TurboMaths.GetEvenNumbers(maxNumber))
+            testList.Add(number);
+        
+        
+        if (testList.Count > maxNumber / 2 + 1) Assert.Fail();
+        if (maxNumber < 0 && testList.Count > 0) Assert.Fail();
+        
+        foreach (int num in testList)
+            if (num % 2 != 0 || num < 0 || num > maxNumber) Assert.Fail();
+    }
+    
+    
 }
--- P201-Algorithms-And-DataStructures/TurboCollections/TurboMaths.cs
+++ P201-Algorithms-And-DataStructures/TurboCollections/TurboMaths.cs
@@ -2,8 +2,21 @@
 
 public static class TurboMaths
 {
-    public static void SayHello()
+    public static void SayHello() => Console.WriteLine($"Hello, I'm {typeof(TurboMaths)}");
+    
+    public static List<int> GetEvenNumbersList(int maxNumber)
     {
-        Console.WriteLine($"Hello, I'm {typeof(TurboMaths)}");
+        List<int> returnValue = new();
+        if (maxNumber < 0) return returnValue;
+        
+        for (int i = 0; i < maxNumber + 1; i += 2)
+            returnValue.Add(i);
+        
+        return returnValue;
+    }
+
+    public static IEnumerable<int> GetEvenNumbers(int maxNumber)
+    {
+        for (int i = 0; i < maxNumber + 1; i += 2) yield return i;
     }
 }
